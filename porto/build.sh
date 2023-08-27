506bcf5cb4cf4d72fd30f41d7fe934b1a2ef4c11
何俊铧 <he.junhua@immomo.com>
2023-08-27 15:43:46 +0800

    build if module has change files

--- porto/build.sh
+++ porto/build.sh
@@ -1,3 +1,7 @@
 cd "$(dirname "$0")" || exit
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.9.jdk/Contents/Home
-mvn clean -Dmaven.test.skip=true package install
+# 获取当前文件夹未提交文件
+modified=$(git status -s ./ | grep -E 'M|A')
+if [ -n "$modified" ]; then
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.9.jdk/Contents/Home
+  mvn clean -Dmaven.test.skip=true package install
+fi
