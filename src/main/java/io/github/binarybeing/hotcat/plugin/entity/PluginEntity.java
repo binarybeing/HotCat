ccb04391368f8f10fde81da65d198fc4b6ecdc77
何俊铧 <he.junhua@immomo.com>
2023-11-08 15:42:14 +0800

    plugin 按 file区别

--- src/main/java/io/github/binarybeing/hotcat/plugin/entity/PluginEntity.java
+++ src/main/java/io/github/binarybeing/hotcat/plugin/entity/PluginEntity.java
@@ -44,11 +44,11 @@ public class PluginEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PluginEntity that = (PluginEntity) o;
-        return Objects.equals(name, that.name);
+        return Objects.equals(file.getAbsolutePath(), that.file.getAbsolutePath());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(file.getAbsolutePath());
     }
 }
